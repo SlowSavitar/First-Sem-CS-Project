dd5053317950f73569bb3948e3940c5349a451e2
Atta Ur Rahman Sheikh <atta032172204842@gmail.com>
2024-12-22 01:07:12 +0500

    Allotment

--- Version-A/Project Main.cpp
+++ Version-A/Project Main.cpp
@@ -12,12 +12,12 @@ int StringToInt(string str)
     return n;
 }
 
-void ReadArray(string students[][750], int &maleStudents, int &femaleStudents, int &totalStudents)
+void ReadArray(string students[][800], int &maleStudents, int &femaleStudents, int &totalStudents)
 {
     ifstream studentFile("Student_Data.txt");
     if (!studentFile)
     {
-        cerr << "ERROR: Student File can't be Opened!";
+        cerr << "Student File can't be Opened!";
         return;
     }
     // [0 = Name] [1 = Reg] [2 = Gender] [3 = Age] [4 = CNIC] [5 = Program] [6 = Section]
@@ -92,14 +92,80 @@ void ReadArray(string students[][750], int &maleStudents, int &femaleStudents, i
     studentFile.close();
 }
 
+void AllotHostels(string students[][800], int totalStudents)
+{
+    const int maleHostelCapacity = 650;
+    const int femaleHostelCapacity = 150;
+    const int roomCapacity = 2;
+
+    int maleCount = 0, femaleCount = 0;
+
+    ofstream studentFile("Updated_Student_Data.txt");
+    if (!studentFile)
+    {
+        cerr << "\033[31m\nUnable to write to the file!\033[0m";
+        return;
+    }
+
+    for (int i = 0; i < totalStudents; i++)
+    {
+        if (students[2][i] == "M")
+        {
+            if (maleCount < maleHostelCapacity)
+            {
+                string hostel = (maleCount < 325 * roomCapacity) ? "H11" : "H12";
+                int roomNumber = (maleCount / roomCapacity) % 325 + 1;
+                students[10][i] = hostel;
+                students[11][i] = to_string(roomNumber);
+                maleCount++;
+            }
+            else
+            {
+                cerr << "\033[31m\nNo available rooms for male students.\n\033[0m";
+            }
+        }
+        else if (students[2][i] == "F")
+        {
+            if (femaleCount < femaleHostelCapacity)
+            {
+                students[10][i] = "GH";
+                students[11][i] = to_string(femaleCount / roomCapacity + 1);
+                femaleCount++;
+            }
+            else
+            {
+                cerr << "\033[31m\nNo available rooms for female students.\n\033[0m";
+            }
+        }
+
+        // Write updated data to the file
+        studentFile << "Name: " << students[0][i] << "\n";
+        studentFile << "Reg: " << students[1][i] << "\n";
+        studentFile << "Gender: " << students[2][i] << "\n";
+        studentFile << "Age: " << students[3][i] << "\n";
+        studentFile << "CNIC: " << students[4][i] << "\n";
+        studentFile << "Program: " << students[5][i] << "\n";
+        studentFile << "Section: " << students[6][i] << "\n";
+        studentFile << "Account Password: " << students[7][i] << "\n";
+        studentFile << "Contact: " << students[8][i] << "\n";
+        studentFile << "Emergency Contact: " << students[9][i] << "\n";
+        studentFile << "Hostel: " << students[10][i] << "\n";
+        studentFile << "Room Number: " << students[11][i] << "\n";
+        studentFile << "---\n";
+    }
+
+    studentFile.close();
+    cout << "\033[32mHostel allocation completed successfully!\033[0m\n";
+}
+
 int DisplayLoginChoice()
 {
     int choice;
     while (true)
     {
-        cout << "\nSelect an option:\n\n";
+        cout << "\nSelect an option:\n\n\033[40m\033[1;33m";
         cout << " 1. Student login\n";
-        cout << " 2. Admin login\n";
+        cout << " 2. Admin login\033[0m\n";
         cout << "\nInput (1 - 2):  ";
         cin >> choice;
 
@@ -120,27 +186,33 @@ int LoginAccount(int choice, string *students, int maxStudents)
     if (choice == 1)
     {
         string regNo, password;
-        cout << "\nEnter Registration Number: ";
-        cin >> regNo;
-        for (int i = 0; i < maxStudents; i++)
+        bool reg = false;
+        do
         {
-            if (*(students + 1 * maxStudents + i) == regNo)
+            cout << "\nEnter Registration Number: ";
+            cin >> regNo;
+            for (int i = 0; i < maxStudents; i++)
             {
-                cout << "Enter Password: ";
-                cin >> password;
-                if (*(students + 7 * maxStudents + i) == password)
+                if (*(students + 1 * maxStudents + i) == regNo)
                 {
-                    return StringToInt(regNo);
-                }
-                else
-                {
-                    cout << "\033[31m\nInvalid password, please try again.\n\033[0m";
-                    LoginAccount(choice, students, maxStudents);
+                    reg = true;
+                    do
+                    {
+                        cout << "Enter Password: ";
+                        cin >> password;
+                        if (*(students + 7 * maxStudents + i) == password)
+                        {
+                            return i;
+                        }
+                        else
+                        {
+                            cout << "\033[31m\nInvalid password, please try again.\n\n\033[0m";
+                        }
+                    } while (true);
                 }
             }
-        }
-        cout << "\033[31m\nInvalid registration number, please try again.\n\033[0m";
-        LoginAccount(choice, students, maxStudents);
+            cout << "\033[31m\nInvalid registration number, please try again.\n\033[0m";
+        } while (!reg);
     }
     else if (choice == 2)
     {
@@ -148,25 +220,49 @@ int LoginAccount(int choice, string *students, int maxStudents)
     }
 }
 
-int DispayFeatures()
+void DisplayStudentProfile(string *students, int maxStudents, int currentAccount)
 {
+}
+
+void DispayStudentFeatures(string *students, int maxStudents, int currentAccount)
+{
+    string name = "\033[1;36m" + *(students + 0 * maxStudents + currentAccount) + "\033[0m";
+    // cout << "\033[1;36mCYAN\033[0m\n";
+    cout << "\nWelcome, " << name << "! Explore the features below to manage your hostel life easily and efficiently.\n";
+
     int choice;
     while (true)
     {
-        cout << "\nSelect an option:\n\n";
-        cout << " 1. Student login\n";
-        cout << " 2. Admin login\n";
-        cout << "\nInput (1 - 2):  ";
+        cout << "\nSelect an option:\n\n\033[40m\033[1;33m";
+        cout << " 1. My Profile\n";
+        cout << " 2. Hostel Overview\n";
+        cout << " 3. Change Password\n";
+        cout << " 4. Room Transfer Request\n";
+        cout << " 5. Parcel Alerts\n";
+        cout << " 6. My Notifications\n";
+        cout << " 7. Admin Announcements\n";
+        cout << " 8. Complaint Tracker\n";
+        cout << " 9. Log Out\033[0m\n";
+        cout << "\nInput (1 - 9):  ";
         cin >> choice;
 
-        if (choice < 1 || choice > 2)
+        if (choice < 1 || choice > 9)
         {
             cout << "\033[31m\nInvalid input, please try again.\n\033[0m";
         }
         else
         {
-            return choice;
+            break;
         }
+    }
+    switch (choice)
+    {
+    case 1:
+        DisplayStudentProfile((string *)students, maxStudents, currentAccount);
+        break;
+
+    default:
+        break;
     }
 }
 
@@ -174,17 +270,24 @@ int main()
 {
     cout << "\033[40m\033[1;33m\t=-=-=-=-=-=-=-=-=-=-=-=\n\t|  HOSTEL MANAGEMENT  |\n\t=-=-=-=-=-=-=-=-=-=-=-=\033[0m\n";
 
+    // cout << "\033[1;32mGREEN\033[0m\n";
+    // cout << "\033[1;36mCYAN\033[0m\n";
+    // cout << "\033[1;34mBLUE\033[0m\n\n";
+
     int maleStudents = 0, femaleStudents = 0, totalStudents = 0;
-    const int maxStudents = 750;
+    const int maxStudents = 800;
     string students[12][maxStudents];
     ReadArray(students, maleStudents, femaleStudents, totalStudents);
+
+    AllotHostels(students, totalStudents);
+
     int currentAccount = LoginAccount(DisplayLoginChoice(), (string *)students, maxStudents);
-    int p = DispayFeatures();
+    DispayStudentFeatures((string *)students, maxStudents, currentAccount);
 
     // [0 = Name] [1 = Reg] [2 = Gender] [3 = Age] [4 = CNIC] [5 = Program] [6 = Section]
     // [7 = Password] [8 = Contact] [9 = Emergency Contact] [10 = Hostel] [11 = Room Number]
 
-    cout << "Total Students: " << totalStudents << endl;
+    cout << "\nTotal Students: " << totalStudents << endl;
     cout << "Male Students: " << maleStudents << endl;
     cout << "Female Students: " << femaleStudents << endl;
 
