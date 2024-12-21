c366f6156f55a28752da090f89d79c7b19496c63
Atta Ur Rahman Sheikh <atta032172204842@gmail.com>
2024-12-21 15:26:08 +0500

    Final Data for Project

--- Version-A/Project Main.cpp
+++ Version-A/Project Main.cpp
@@ -1,8 +1,23 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 using namespace std;
 
 int main()
 {
-    cout << "CS PROJECT MAIN";
+    ifstream studentFile("Student_Data.txt");
+    if (!studentFile)
+    {
+        cerr << "ERROR: Student File can't be Opened!";
+        return 1;
+    }
+    string line;
+    while (getline(studentFile, line))
+    {
+        if (line.find("Name: ") == 0)
+        {                                   
+            cout << line.substr(6) << endl; 
+        }
+    }
     return 0;
 }
